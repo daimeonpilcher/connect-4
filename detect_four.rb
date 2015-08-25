eba6318088b9306a7e8f0caba7ebeb54c9b672b6
Daimeon Pilcher <daimeonpilcher@Daimeons-MacBook-Pro.local>
2015-08-25 15:36:44 -0700

    DetectFour completed

--- detect_four.rb
+++ detect_four.rb
@@ -21,9 +21,46 @@ class DetectFour
 
   def count_on(axis)
     send axis
+
   end
 
   def connect_four?
     @connected >= 4
+  end
+
+  def vertical
+    count :center, :down
+  end
+
+  def horizontal
+    count :left, :center
+    count :right, :center
+  end
+
+  def right_diag
+    count :left, :up
+    count :right, :down
+  end
+
+  def left_diag
+    count :up, :right
+    count :down, :left
+  end
+
+  def count (x_direction, y_direction)
+    @cursor = last_move
+    @cursor.move(x_direction, y_direction)
+    while @cursor.within_bounds?
+      color_match ? @connected += 1 : break
+      @cursor.move(x_direction, y_direction)
+    end
+  end
+
+  def last_move
+    Position.new(@board.last_x, @board.last_y)
+  end
+
+  def color_match
+    @board.color_of(@cursor.x, @cursor.y) == @board.last_color
   end
 end
